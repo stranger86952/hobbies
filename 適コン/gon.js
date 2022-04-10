9e07d012ce85a5016fa83b4648f46c17ea4deb22
stranger86952 <90621530+stranger86952@users.noreply.github.com>
2022-04-10 16:21:13 +0900

    Update gon.js

--- 適コン/gon.js
+++ 適コン/gon.js
@@ -1,1 +1,3 @@
-$('.ooo').load('https://stranger86952.github.io/hobbies/%E9%81%A9%E3%82%B3%E3%83%B3/contestinfo.html table');
+window.addEventHandler("load", function() {
+  $('.ooo').load('https://stranger86952.github.io/hobbies/%E9%81%A9%E3%82%B3%E3%83%B3/contestinfo.html table');
+})
